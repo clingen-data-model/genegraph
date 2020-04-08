807d3d1d05bdf5b4cc3807232c4a8093d9fb7e7b
Tristan Nelson <tristan@iseult.us>
2020-04-08 17:07:40 -0400

    WIP building gene validity evidence scaffold

--- src/genegraph/transform/gene_validity.clj
+++ src/genegraph/transform/gene_validity.clj
@@ -15,10 +15,11 @@
                   "sepio" "http://purl.obolibrary.org/obo/SEPIO_"
                   "dc" "http://purl.org/dc/terms/"
                   "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
-                  "evidence_item" "http://dataexchange.clinicalgenome.org/gci/evidence_item/"
-                  "evidence_line" "http://dataexchange.clinicalgenome.org/gci/evidence_line/"
-                  "proposition" "http://dataexchange.clinicalgenome.org/gci/proposition/"
-                  "assertion" "http://dataexchange.clinicalgenome.org/gci/assertion/"
+                  ;; "evidence_item" "http://dataexchange.clinicalgenome.org/gci/evidence_item/"
+                  ;; "evidence_line" "http://dataexchange.clinicalgenome.org/gci/evidence_line/"
+                  ;; "proposition" "http://dataexchange.clinicalgenome.org/gci/proposition/"
+                  ;; "assertion" "http://dataexchange.clinicalgenome.org/gci/assertion/"
+                  "dxgci" "http://dataexchange.clinicalgenome.org/gci/"
                   "ro" "http://purl.obolibrary.org/obo/RO_"
                   "mondo" "http://purl.obolibrary.org/obo/MONDO_"
                   "car" "http://reg.genome.network/allele/"
@@ -67,12 +68,6 @@
     (l/read-rdf is {:format :json-ld})))
 
 
-;; TODO, this is incomplete, need more data on this
-;; ask at next data model meeting
-(def case-info-type-to-sepio-type
-  {"PREDICTED_OR_PROVEN_NULL_VARIANT" :sepio/fixme
-   "OTHER_VARIANT_TYPE_WITH_GENE_IMPACT" :sepio/fixme })
-
 (defn transform-gdm [gdm]
   (.setNsPrefixes gdm ns-prefixes)
   (let [params {::q/model (q/union gdm gdm-sepio-relationships)
@@ -82,4 +77,5 @@
                 :pmbase "https://pubmed.ncbi.nlm.nih.gov/"}]
     (q/union (construct-proposition params)
              (construct-evidence-level-assertion params)
-             (construct-proband-score params))))
+             ;;(construct-proband-score params)
+             )))
