a0a6afafd6b9eda5bd00cd39859c44620f052ed0
Tristan Nelson <tristan@iseult.us>
2020-05-12 09:32:42 -0400

    WIP adding necessary fields for gene, disease detail

--- src/genegraph/source/graphql/gene_validity.clj
+++ src/genegraph/source/graphql/gene_validity.clj
@@ -1,13 +1,13 @@
 (ns genegraph.source.graphql.gene-validity
-  (:require [genegraph.database.query :as q]))
+  (:require [genegraph.database.query :as q :refer [ld-> ld1-> create-query]]))
 
 (defn report-date [context args value]
   "")
 
-(defn wg-label [context args value]
-  "")
+(defn classification [context args value]
+  :NO_KNOWN_DISEASE_RELATIONSHIP)
 
-(defn classification-description [context args value]
+(defn gene [context args value]
   "")
 
 
