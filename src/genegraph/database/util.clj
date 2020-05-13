8b86a2a068edc1b6832722cb43824cdce4e2c981
Tristan Nelson <tristan@iseult.us>
2020-05-13 10:24:34 -0400

    WIP creating transaction start, end functions without wrapping macro

--- src/genegraph/database/util.clj
+++ src/genegraph/database/util.clj
@@ -11,6 +11,7 @@
  (ResourceFactory/createProperty uri))
 
 (def ^:dynamic *in-tx* false)
+(def ^:dynamic *current-union-model* nil)
 
 (defmacro tx 
   "Open a read transaction on the persistent database. Most commands that read data from the databse will call this internally, since Jena TDB explicitly requires opening a transaction to read any data. If one wishes to issue multiple read commands within the scope of a single transaction, it is perfectly acceptable to wrap them all in a tx call, as this uses the var *in-tx* to ensure only a single transaction per thread is opened."
@@ -37,6 +38,12 @@
            result#)
          (catch Exception e# (log/error :fn :tx :msg e#) (.abort db))
          (finally (.end db))))))
+
+(defn begin-read-tx
+  "Open a read transaction on the persistent database and leave it open within the context of the current thread. When possible, the macro form is preferred, however this fn is available when one does not have access to the block of code to be called in the context of a transaction (i.e. in a Pedestal interceptor"
+  (.begin db ReadWrite/READ)
+  
+  ())
 
 (defmacro with-test-database 
   "Uses with-redefs to replace reference to production database with temporary,
