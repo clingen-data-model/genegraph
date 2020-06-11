e1785b748a7644bbe397387470969f3cf6efbc3f
Tristan Nelson <tristan@iseult.us>
2020-06-11 12:40:25 -0400

    Cleaning up dependency tree with exclusions

--- project.clj
+++ project.clj
@@ -4,29 +4,30 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.2.603"]
-                 [io.pedestal/pedestal.service "0.5.5"]
-                 [io.pedestal/pedestal.jetty "0.5.5"]
+                 [org.clojure/core.async "1.2.603"  :exclusions [org.clojure/tools.reader]]
+                 [io.pedestal/pedestal.service "0.5.8"]
+                 [io.pedestal/pedestal.jetty "0.5.8"]
                  [hiccup "1.0.5"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [org.slf4j/jul-to-slf4j "1.7.26"]
+                 [org.slf4j/jcl-over-slf4j "1.7.26"]
+                 [org.slf4j/log4j-over-slf4j "1.7.26"]
                  [org.apache.kafka/kafka-clients "2.3.0"]
                  [cheshire "5.8.1"]
                  [org.clojure/data.csv "0.1.4"]
                  [camel-snake-kebab "0.4.0"]
                  ;; jena 3.14.0 includes lucene 7.4.0
-                 [org.apache.jena/jena-core "3.14.0"]
-                 [org.apache.jena/jena-arq "3.14.0"]
-                 [org.apache.jena/jena-iri "3.14.0"]
-                 [org.apache.jena/jena-tdb2 "3.14.0"]
-                 [org.apache.jena/jena-text "3.14.0" :exclusions [org.apache.jena/jena-cmds]]
+[org.apache.jena/jena-text "3.14.0" :exclusions [org.slf4j/jcl-over-slf4j commons-codec org.slf4j/slf4j-api org.apache.commons/commons-compress org.apache.jena/jena-cmds org.apache.httpcomponents/httpcore com.fasterxml.jackson.core/jackson-core]]
+[org.apache.jena/jena-tdb2 "3.14.0" :exclusions [org.slf4j/jcl-over-slf4j commons-codec org.slf4j/slf4j-api org.apache.commons/commons-compress org.apache.httpcomponents/httpcore com.fasterxml.jackson.core/jackson-core]]
+[org.apache.jena/jena-arq "3.14.0" :exclusions [org.slf4j/jcl-over-slf4j commons-codec org.slf4j/slf4j-api org.apache.commons/commons-compress org.apache.httpcomponents/httpcore com.fasterxml.jackson.core/jackson-core]]
+[org.apache.jena/jena-core "3.14.0" :exclusions [commons-codec org.slf4j/slf4j-api org.apache.commons/commons-compress]]
+[org.apache.jena/jena-iri "3.14.0" :exclusions [org.slf4j/slf4j-api]]
+[org.apache.kafka/kafka-clients "2.3.0" :exclusions [org.slf4j/slf4j-api]]
                  [org.topbraid/shacl "1.1.0"]
                  [mount "0.1.14"]
                  [com.velisco/clj-ftp "0.3.9"] ;; For downloading ftp docs
                  [clj-http "3.9.1"] ;; For downloading 
-                 [com.walmartlabs/lacinia-pedestal "0.12.0"]
+                 [com.walmartlabs/lacinia-pedestal "0.12.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [clj-commons/fs "1.5.2"]
                  ;; Dirwatch for updating base files in development
                  [juxt/dirwatch "0.2.5"]
@@ -40,9 +41,7 @@
                  [org.openjfx/javafx-base "11.0.1"]
                  [org.openjfx/javafx-web "11.0.1"]
                  [expound "0.7.2"]
-                 [com.fasterxml.jackson.core/jackson-core "2.10.2"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.10.2"]
-                 [com.google.cloud/google-cloud-storage "1.108.0"]]
+                 [com.google.cloud/google-cloud-storage "1.108.0" :exclusions [org.apache.httpcomponents/httpclient org.apache.httpcomponents/httpcore com.fasterxml.jackson.core/jackson-core]]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources", "jars/REBL-0.9.220.jar"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
