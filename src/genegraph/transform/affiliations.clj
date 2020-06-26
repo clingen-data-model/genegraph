26a49292931fb798c20d15c52bcf612320384366
Tristan Nelson <tristan@iseult.us>
2020-06-26 10:54:29 -0400

    Adding affiliatons to base import

---
+++ src/genegraph/transform/affiliations.clj
@@ -0,0 +1,24 @@
+(ns genegraph.transform.affiliations
+  (:require [clojure.data.csv :as csv]
+            [clojure.string :as s]
+            [genegraph.database.load :as l]
+            [genegraph.transform.core :refer [transform-doc src-path]]))
+
+(def affiliation-prefix "http://dataexchange.clinicalgenome.org/agent/")
+
+(defn affiliation-to-triples [affiliation-row]
+  (let [[label id] affiliation-row
+        iri (str affiliation-prefix id)]
+    [[iri :skos/preferred-label (s/trim label)]
+     [iri :rdf/type :cg/Affiliation]]))
+
+(defn affiliations-to-triples [affiliations-csv]
+  (mapcat affiliation-to-triples (rest affiliations-csv)))
+
+(defmethod transform-doc :affiliations [doc-def]
+  (->> doc-def 
+       src-path
+       slurp
+       csv/read-csv
+       affiliations-to-triples
+       l/statements-to-model))
