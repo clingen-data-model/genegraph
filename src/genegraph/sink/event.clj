827a4488b8c9a4dc4cf8415564befc7cf58a7a6a
Tristan Nelson <tristan@iseult.us>
2020-07-28 11:13:41 -0400

    WIP processing neo4j gene validity export

--- src/genegraph/sink/event.clj
+++ src/genegraph/sink/event.clj
@@ -1,8 +1,12 @@
 (ns genegraph.sink.event
   (:require [genegraph.database.query :as q]
             [genegraph.database.load :refer [load-model]]
-            [genegraph.annotate :as annotate :refer [add-model add-iri add-metadata]]
-            [genegraph.source.graphql.common.cache :as cache]))
+            [genegraph.annotate :as annotate :refer 
+             [add-model add-iri add-metadata]]
+            [genegraph.source.graphql.common.cache :as cache]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import (java.io PushbackReader File)))
 
 (defn add-to-db! [event]
   (load-model (::q/model event) (::annotate/iri event))
@@ -16,3 +20,12 @@
                             add-to-db!)]
     (cache/reset-cache!)
     processed-event))
+
+(defn process-directory! 
+  "Read and integrate a directory full of event records"
+  [path]
+  (let  [dir (File. path)
+         files (file-seq dir)]
+    (doseq [f files]
+      (with-open [evt (-> f io/reader PushbackReader. edn/read)]
+        (process-event! evt)))))
