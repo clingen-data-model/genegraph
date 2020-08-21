2daf1b521eec314ecf13591e34c8c957229380b7
Tristan Nelson <tristan@iseult.us>
2020-08-21 16:00:58 -0400

    Refactoring algebra operations into separate ns

--- src/genegraph/database/query/resource.clj
+++ src/genegraph/database/query/resource.clj
@@ -2,6 +2,7 @@
   (:require [genegraph.database.instance :refer [db]]
             [genegraph.database.util :as util :refer [tx]]
             [genegraph.database.query.types :as types]
+            [genegraph.database.query.algebra :as algebra]
             [genegraph.database.names :as names :refer
              [local-class-names local-property-names
               class-uri->keyword local-names ns-prefix-map
@@ -214,114 +215,6 @@
     (doseq [model models] (.add union-model model))
     union-model))
 
-
-;;;; Query builder
-
-(defn text-index [var ])
-
-(defn triple
-  "Construct triple for use in BGP. Part of query algebra."
-  [stmt]
-  (let [[s p o] stmt
-        subject (cond
-                  (instance? Node s) s
-                  (= '_ s) Var/ANON
-                  (symbol? s) (Var/alloc (str s))
-                  (keyword? s) (.asNode (local-class-names s))
-                  (string? s) (NodeFactory/createURI s)
-                  (types/resource? o) (.asNode (as-jena-resource s))
-                  :else (NodeFactory/createURI (str s)))
-        predicate (cond
-                    (instance? Node p) p
-                    (keyword? p) (.asNode (local-property-names p))
-                    :else (NodeFactory/createURI (str p)))
-        object (cond 
-                 (instance? Node o) o
-                 (symbol? o) (Var/alloc (str o))
-                 (keyword? o) (.asNode (or (local-class-names o) (local-property-names o)))
-                 (or (string? o)
-                     (int? o)
-                     (float? o)) (.asNode (ResourceFactory/createTypedLiteral o))
-                 (types/resource? o) (.asNode (as-jena-resource o))
-                 :else o)]
-    (Triple. subject predicate object)))
-
-;; (defn expr
-;;   "Convert a Clojure data structure to an Arq Expr"
-;;   [expr]
-;;   (if (instance? java.util.List expr)
-;;     (composite-expr expr)
-;;     (let [node (graph/node expr)]
-;;       (if (instance? Node_Variable node)
-;;         (ExprVar. (Var/alloc ^Node_Variable node))
-;;         (NodeValue/makeNode node)))))
-
-;; (defn- var-expr-list
-;;   "Given a vector of var/expr bindings (reminiscient of Clojure's `let`), return a Jena VarExprList with vars and exprs."
-;;   [bindings]
-;;   (let [vel (VarExprList.)]
-;;     (doseq [[v e] (partition 2 bindings)]
-;;       (.add vel (Var/alloc (graph/node v))
-;;         (expr e)))
-;;     vel))
-
-;; (defn- var-aggr-list
-;;   "Given a vector of var/aggregate bindings return a Jena VarExprList with
-;;    vars and aggregates"
-;;   [bindings]
-;;   (vec (for [[v e] (partition 2 bindings)]
-;;          (ExprAggregator. (Var/alloc (graph/node v)) (aggregator e)))))
-
-;; (defn- sort-conditions
-;;   "Given a seq of expressions and the keyword :asc or :desc, return a list of
-;;    sort conditions."
-;;   [conditions]
-;;   (for [[e dir] (partition 2 conditions)]
-;;     (SortCondition. ^Expr (expr e) (if (= :asc dir) 1 -1))))
-
-(defn- var-seq [vars]
-  (map #(Var/alloc (str %)) vars))
-
-(declare op)
-
-(defn- op-union [a1 a2 & amore]
-  (OpUnion. 
-   (op a1)
-   (if amore
-     (apply op-union a2 amore)
-     (op a2))))
-
-(defn op
-  "Convert a Clojure data structure to an Arq Op"
-  [[op-name & [a1 a2 & amore :as args]]]
-  (case op-name
-    :distinct (OpDistinct/create (op a1))
-    :project (OpProject. (op a2) (var-seq a1))
-    ;; :filter (OpFilter/filterBy (ExprList. ^List (map expr (butlast args))) (op (last args)))
-    :bgp (OpBGP. (BasicPattern/wrap (map triple args)))
-    :conditional (OpConditional. (op a1) (op a2))
-    :diff (OpDiff/create (op a1) (op a2))
-    :disjunction (OpDisjunction/create (op a1) (op a2))
-    ;; :extend (OpExtend/create (op a2) (var-expr-list a1))
-    ;; :group (OpGroup/create (op (first amore))
-    ;;                        (VarExprList. ^List (var-seq a1))
-    ;;                        (var-aggr-list a2))
-    :join (OpJoin/create (op a1) (op a2))
-    :label (OpLabel/create a1 (op a2))
-    ;; :left-join (OpLeftJoin/create (op a1) (op a2) (ExprList. ^List (map expr amore)))
-    :list (OpList. (op a1))
-    :minus (OpMinus/create (op a1) (op a2))
-    :null (OpNull/create)
-    ;; :order (OpOrder. (op a2) (sort-conditions a1))
-    :reduced (OpReduced/create (op a1))
-    :sequence (OpSequence/create (op a1) (op a2))
-    :slice (OpSlice. (op a1) (long a1) (long (first amore)))
-    ;; :top-n (OpTopN. (op (first amore)) (long a1) (sort-conditions a2))
-    :union (apply op-union args)
-    (throw (ex-info (str "Unknown operation " op-name) {:op-name op-name
-                                                        :args args}))))
-
-
 (defn- compose-select-result [qexec model]
   (when-let [result (-> qexec .execSelect)]
     (let [result-var (-> result .getResultVars first)
@@ -359,7 +252,7 @@ use io/slurp"
   ([query-source] (create-query query-source {}))
   ([query-source params]
    (let [query (if  (coll? query-source)
-                  (OpAsQuery/asQuery (op query-source))
+                  (OpAsQuery/asQuery (algebra/op query-source))
                   (QueryFactory/create (expand-query-str
                                         (if (string? query-source)
                                           query-source
