bc6936c0834a628f94a86db123da78d4f4b10d7b
Tristan Nelson <tristan@iseult.us>
2020-08-27 09:53:58 -0400

    Use only subclasses of HP/MOI

--- src/genegraph/source/graphql/mode_of_inheritance.clj
+++ src/genegraph/source/graphql/mode_of_inheritance.clj
@@ -6,7 +6,8 @@
   (q/create-query 
    "select distinct ?moi where 
 { ?prop a :sepio/GeneValidityProposition .
-  ?prop :sepio/has-qualifier ?moi }"))
+  ?prop :sepio/has-qualifier ?moi .
+  ?moi <http://www.w3.org/2000/01/rdf-schema#subClassOf>* <http://purl.obolibrary.org/obo/HP_0000005> }"))
 
 (defresolver modes-of-inheritance [args value]
   (modes-of-inheritance-query))
