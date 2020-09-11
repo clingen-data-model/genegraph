4996bb2d5603c66c7e9f41fd195f80f24bcb2d31
Terry ONeill <toneill@broadinstitute.org>
2020-09-11 13:56:23 -0400

    Modified migration to include the interceptor chain Added logging messages in place of println Changed logger configuration to include DEBUG level messages

--- src/genegraph/sink/batch.clj
+++ src/genegraph/sink/batch.clj
@@ -5,7 +5,8 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [me.raynes.fs :as fs]
-            [clojure.java.shell :refer [sh]])
+            [clojure.java.shell :refer [sh]]
+            [io.pedestal.log :as log])
   (:import (java.io PushbackReader File)))
 
 (def batch-events "batch-events.edn")
@@ -18,10 +19,10 @@
     (doseq [f files]
       (with-open [rdr (io/reader f)
                   pushback-rdr (PushbackReader. rdr)]
-        (println "processing: " f)
+        (log/debug :fn :process-directory! :msg (str "processing: " f))
         (event/process-event! (edn/read pushback-rdr))))))
 
-(defn proccess-batched-events! 
+(defn process-batched-events! 
   "Should be run during database initialization. Download and read events stored in batch format into database."
   []
   (let [target-dir (str env/data-vol "/events/")
@@ -29,7 +30,7 @@
     (doseq [batch batches]
       (let [batch-dir (str target-dir (:name batch)) 
             archive-path (str batch-dir ".tar.gz")]
-        (println (:name batch))
+        (log/debug :fn :process-batched-events! :msg (:name batch))
         (fs/mkdirs batch-dir)
         (gcs/get-file-from-bucket! (:source batch) archive-path)
         (sh "tar" "-xzf" archive-path "-C" batch-dir)
