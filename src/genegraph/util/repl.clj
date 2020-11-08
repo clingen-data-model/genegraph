9d276a7e77e3a71930d21d19d63c978fb1a95855
Tristan Nelson <tristan@iseult.us>
2020-11-08 06:49:31 -0500

    Adding helpers for testing at repl

---
+++ src/genegraph/util/repl.clj
@@ -0,0 +1,13 @@
+(ns genegraph.util.repl
+  "Functions and environment useful for developing Genegraph at the REPL"
+  (:require [genegraph.database.query :as q]
+            [genegraph.database.load :as l]
+            [genegraph.sink.stream :as stream]
+            [genegraph.annotate :as ann]))
+
+(defn clear-named-grpahs-with-type [type-carrying-graph-name]
+  (let [named-graphs (map str
+                          (q/select "select ?graph where { ?graph a ?type } " 
+                                    {:type type-carrying-graph-name}))]
+    (doseq [graph-name named-graphs]
+      (l/remove-model graph-name))))
