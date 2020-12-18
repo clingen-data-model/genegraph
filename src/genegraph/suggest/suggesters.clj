ef7ac530930c8bda530a595fecf5af6825650074
Terry ONeill <toneill@broadinstitute.org>
2020-12-18 11:33:23 -0500

    Changed migration to build suggestions at the end of migration. Fixed suggesters to update when there is a curation change. Modified when suggesters are committed and refreshed on build.

--- src/genegraph/suggest/suggesters.clj
+++ src/genegraph/suggest/suggesters.clj
@@ -128,15 +128,15 @@
                                       (:label payload)
                                       payload
                                       (:curations payload)
-                                      (:weight payload))
-          (suggest/commit-suggester suggester)
-          (suggest/refresh-suggester suggester))
-        (log/debug :fn :build-suggestions :suggester key :msg :complete)))))
+                                      (:weight payload)))))
+    suggester))
                                     
 (defn build-all-suggestions []
   "Build all of the suggester indices for all configured suggesters"
   (doseq [suggester-key (keys @suggesters)]
-    (build-suggestions suggester-key)))
+      (let [suggester (build-suggestions suggester-key)]
+        (suggest/commit-suggester suggester)
+        (suggest/refresh-suggester suggester))))
 
 (defn get-suggester [key]
   "Retreive a suggester from the suggester atom" 
@@ -149,20 +149,35 @@
     (suggest/lookup (get-suggester suggester-key) text contexts num)
     nil))
 
+(defn get-suggester-result-map [resource-iri suggester-key]
+  (let [resource (q/resource resource-iri)
+        label (label resource)
+        lookup-results (lookup suggester-key label #{:ALL} 20)] ;; how to exact-match?
+    (if-let [lookup-result (first (filter #(= label (.key %)) lookup-results))]
+      (let [payload (-> (.payload lookup-result) .bytes serder/deserialize)]
+        (if (= resource-iri (:iri payload))
+          {:lookup-result lookup-result :resource resource :payload payload}
+          nil))
+      nil)))
+
 (defn process-event-resource! [resource-iri suggester-type]
   (log/debug :fn :process-event-resource! :resource-type suggester-type :resource-iri resource-iri)
-  (let [suggester (get-suggester suggester-type)
-        suggest-map (get (suggesters-map) suggester-type)
-        resource (q/resource resource-iri)
-        new-payload ((suggest-map :payload) resource)]
-    (when (some? (:label new-payload))
-      (suggest/update-suggestion suggester
-                                 (:label new-payload)
-                                 new-payload
-                                 (:curations new-payload)
-                                 (:weight new-payload))
-      (suggest/commit-suggester suggester)
-      (suggest/refresh-suggester suggester))))
+  (when-let [resource-payload-map (get-suggester-result-map resource-iri suggester-type)]
+    (let [suggester (get-suggester suggester-type)
+          suggest-map (get (suggesters-map) suggester-type)
+          resource (q/resource resource-iri)
+          old-payload (:payload resource-payload-map)
+          new-payload ((suggest-map :payload) resource)]
+      (when (and (some? (:label new-payload))
+                 (not (= (:curations old-payload) (:curations new-payload))))
+        (suggest/update-suggestion suggester
+                                   (:label new-payload)
+                                   new-payload
+                                   (:curations new-payload)
+                                   (:weight new-payload))
+        (suggest/commit-suggester suggester)
+        (suggest/refresh-suggester suggester)
+        (log/debug :fn :process-event-resource! :suggester suggester-type :text (:label new-payload) :msg :updated)))))
 
 (defn update-suggesters [event]
   (log/debug :fn :update-suggesters :event event :msg :received-event)
@@ -175,7 +190,8 @@
 
 (def update-suggesters-interceptor
   "Interceptor for updating gene and disease suggesters with curation activities"
-  (interceptor-enter-def ::update-suggesters update-suggesters))
+  {:name ::update-suggesters
+   :enter update-suggesters})
          
 (defstate suggestions
   :start (create-suggesters)
