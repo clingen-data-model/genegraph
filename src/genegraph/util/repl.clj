14eaace1a62a315f12e079de344a0d9d1d42b872
Tristan Nelson <thnelson@geisinger.edu>
2021-06-03 08:49:01 -0400

    Adding keys-in fm for investigating maps

--- src/genegraph/util/repl.clj
+++ src/genegraph/util/repl.clj
@@ -223,3 +223,14 @@
          (csv/write-csv w))))
 
 
+(defn keys-in [m]
+  (if (map? m)
+    (vec 
+     (mapcat (fn [[k v]]
+               (let [sub (keys-in v)
+                     nested (map #(into [k] %) (filter (comp not empty?) sub))]
+                 (if (seq nested)
+                   nested
+                   [[k]])))
+             m))
+    []))
