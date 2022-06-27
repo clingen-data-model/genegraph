5f95ac19c6218c5cd531380b43493f5c53ae4e6a
Kyle Ferriter <kferrite@broadinstitute.org>
2022-06-27 16:18:22 -0400

    Remove comment from requires. Whitespace

--- src/genegraph/transform/core.clj
+++ src/genegraph/transform/core.clj
@@ -18,7 +18,6 @@
             ;; [genegraph.transform.gene-validity]
             [genegraph.transform.gene-validity-refactor]
             [genegraph.transform.actionability]
-            ;; need to fix to be compatible with latest jena =tristan
             [genegraph.transform.clinvar.core]
             [genegraph.transform.rxnorm]
             [genegraph.env :as env]
@@ -27,33 +26,31 @@
 (defn add-model [event]
   (try
     (xform-types/add-model event)
-    (catch Exception e 
+    (catch Exception e
       (assoc event :exception (str e)))))
 
 (defn transform-doc [doc]
   (xform-types/transform-doc doc))
 
-(defmethod xform-types/transform-doc :rdf [doc-def] 
+(defmethod xform-types/transform-doc :rdf [doc-def]
   (with-open [is (if (:target doc-def)
                    (io/input-stream (str (xform-types/target-base) (:target doc-def)))
-                   (-> doc-def :document .getBytes java.io.ByteArrayInputStream.))] 
+                   (-> doc-def :document .getBytes java.io.ByteArrayInputStream.))]
     (l/read-rdf is (:reader-opts doc-def))))
 
 (defn add-model-with-format [event format]
-  (with-open [is (cond 
+  (with-open [is (cond
                    (:genegraph.sink.event/value event)
                    (-> event :genegraph.sink.event/value .getBytes java.io.ByteArrayInputStream.)
                    (:genegraph.sink.base/document event)
-                   (io/input-stream (str (xform-types/target-base) (:genegraph.sink.base/document event))))] 
+                   (io/input-stream (str (xform-types/target-base) (:genegraph.sink.base/document event))))]
     (assoc event :genegraph.database.query/model (l/read-rdf is {:format format}))))
 
-(defmethod xform-types/add-model :rdf-xml [event] 
+(defmethod xform-types/add-model :rdf-xml [event]
   (add-model-with-format event :rdf-xml))
 
-(defmethod xform-types/add-model :json-ld [event] 
+(defmethod xform-types/add-model :json-ld [event]
   (add-model-with-format event :json-ld))
 
-(defmethod xform-types/add-model :turtle [event] 
+(defmethod xform-types/add-model :turtle [event]
   (add-model-with-format event :turtle))
-
-
