a7c90a6a11acbba595d95f93ece4a7a8c4b24989
Kyle Ferriter <kferrite@broadinstitute.org>
2022-08-22 13:46:43 -0400

    Keep vrs normalization cache db open with defstate

--- src/genegraph/transform/clinvar/cancervariants.clj
+++ src/genegraph/transform/clinvar/cancervariants.clj
@@ -1,10 +1,12 @@
 (ns genegraph.transform.clinvar.cancervariants
   (:require [cheshire.core :as json]
-            [clj-http.client :as http]
             [genegraph.database.names :as names :refer [prefix-ns-map]]
+            [genegraph.rocksdb :as rocksdb]
             [genegraph.util.http-client :as ghttp :refer [http-get-with-cache]]
-            [io.pedestal.log :as log])
-  (:import (clojure.lang Keyword)))
+            [io.pedestal.log :as log]
+            [mount.core :refer [defstate]])
+  (:import (clojure.lang Keyword)
+           (org.rocksdb RocksDB)))
 
 (def cancer-variants-normalize-url
   "URL for cancervariants.org VRSATILE normalization.
@@ -41,18 +43,22 @@
 
 (def vicc-db-name "cancervariants-cache.db")
 
+(defstate ^RocksDB vicc-db
+  :start (rocksdb/open vicc-db-name)
+  :stop (rocksdb/close vicc-db))
+
 (defn normalize-spdi [^String spdi-expression]
-  (log/info :fn ::normalize-spdi :variation-expression spdi-expression)
-  (let [response (http-get-with-cache vicc-db-name
+  (log/info :fn :normalize-spdi :variation-expression spdi-expression)
+  (let [response (http-get-with-cache vicc-db
                                       canonical_spdi_to_categorical_variation
                                       {:query-params {"q" spdi-expression}})
         status (:status response)]
     (case status
       200 (let [body (-> response :body json/parse-string)]
-            (log/debug :fn ::vrs-allele-for-variation :body body)
+            (log/debug :fn :vrs-allele-for-variation :body body)
             (-> body (get "categorical_variation") add-vicc-context))
       ;; Error case
-      (log/error :fn ::normalize-spdi :msg "Error in VRS normalization request" :status status :response response))))
+      (log/error :fn :normalize-spdi :msg "Error in VRS normalization request" :status status :response response))))
 
 (defn wrap-with-canonical-variation
   [variation-object]
@@ -62,53 +68,54 @@
 
 (defn normalize-general
   [^String variation-expression]
-  (log/info :fn ::normalize-general :variation-expression variation-expression)
-  (let [response (http-get-with-cache vicc-db-name
+  (log/info :fn :normalize-general :variation-expression variation-expression)
+  (let [response (http-get-with-cache vicc-db
                                       cancer-variants-normalize-url
                                       {:query-params {"q" variation-expression}})
         status (:status response)]
     (case status
       200 (let [body (-> response :body json/parse-string)]
-            (log/debug :fn ::vrs-allele-for-variation :body body)
+            (log/debug :fn :vrs-allele-for-variation :body body)
             (-> body (get "variations") first wrap-with-canonical-variation add-vicc-context))
       ;; Error case
-      (log/error :fn ::normalize-general :msg "Error in VRS normalization request" :status status :response response))))
+      (log/error :fn :normalize-general :msg "Error in VRS normalization request" :status status :response response))))
 
 (defn normalize-canonical
   [^String variation-expression ^Keyword expression-type]
-  (log/info :fn ::normalize-canonical :variation-expression variation-expression)
-  (let [response (http-get-with-cache vicc-db-name
+  (log/info :fn :normalize-canonical :variation-expression variation-expression)
+  (let [response (http-get-with-cache vicc-db
                                       cancer-variants-url-to-canonical
                                       {:query-params {"q" variation-expression
                                                       "fmt" (name expression-type)}})
         status (:status response)]
     (case status
       200 (let [body (-> response :body json/parse-string)]
-            (log/debug :fn ::normalize-canonical :body body)
+            (log/debug :fn :normalize-canonical :body body)
             (-> body (get "canonical_variation") add-vicc-context))
       ;; Error case
-      (log/error :fn ::normalize-canonical :msg "Error in VRS normalization request" :status status :response response))))
+      (log/error :fn :normalize-canonical :msg "Error in VRS normalization request" :status status :response response))))
 
 (defn normalize-absolute-copy-number
   [input-map]
-  (log/info :fn ::normalize-absolute-copy-number :input-map input-map)
-  (let [response (http/get absolute-copy-number-url
-                           {:query-params
-                            (into {}
-                                  (map #(vector (-> % first name) (-> % second))
-                                       (select-keys input-map [:assembly
-                                                               :chr
-                                                               :start
-                                                               :end
-                                                               :total_copies])))})
+  (log/info :fn :normalize-absolute-copy-number :input-map input-map)
+  (let [response (http-get-with-cache vicc-db
+                                      absolute-copy-number-url
+                                      {:query-params
+                                       (into {}
+                                             (map #(vector (-> % first name) (-> % second))
+                                                  (select-keys input-map [:assembly
+                                                                          :chr
+                                                                          :start
+                                                                          :end
+                                                                          :total_copies])))})
         status (:status response)]
     (case status
       200 (let [body (-> response :body json/parse-string)]
-            (log/info :fn ::normalize-absolute-copy-number :body body)
+            (log/info :fn :normalize-absolute-copy-number :body body)
             (assert not-empty (-> body (get "absolute_copy_number")))
             (-> body (get "absolute_copy_number") add-vicc-context))
       ;; Error case
-      (log/error :fn ::normalize-absolute-copy-number
+      (log/error :fn :normalize-absolute-copy-number
                  :msg "Error in VRS normalization request"
                  :status status
                  :response response))))
@@ -120,7 +127,7 @@
   ([^String variation-expression]
    (vrs-variation-for-expression variation-expression nil))
   ([^String variation-expression ^Keyword expression-type]
-   (log/info :fn ::vrs-allele-for-variation :variation-expression variation-expression :expr-type expression-type)
+   (log/debug :fn :vrs-allele-for-variation :variation-expression variation-expression :expr-type expression-type)
    (case expression-type
      :cnv (normalize-absolute-copy-number variation-expression)
      :spdi (normalize-canonical variation-expression :spdi)
