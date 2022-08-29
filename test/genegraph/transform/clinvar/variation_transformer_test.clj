45d85f3e9f5fd91c1ca48adc43213fdf91d07d8f
Kyle Ferriter <kferrite@broadinstitute.org>
2022-08-28 23:45:46 -0400

    Remove dead code from variation_transformer_test.

--- test/genegraph/transform/clinvar/variation_transformer_test.clj
+++ test/genegraph/transform/clinvar/variation_transformer_test.clj
@@ -45,10 +45,7 @@
    ::topic "clinvar-raw"
    ::partition 0
    ;;::offset (.offset consumer-record)
-   }
-  #_{:genegraph.sink.event/value (json/generate-string input-map)
-     :genegraph.transform.core/format :clinvar-raw
-     :genegraph.annotate/graph-name :cg/ClinVarObject})
+   })
 
 (defn file-lazy-seq
   "Lazy line-seq over a whole file. "
@@ -65,8 +62,6 @@
 
 (defn get-variant-messages []
   (-> "clinvar-raw-filtered-variation.txt"
-      #_"clinvar-raw-filtered.txt"
-      #_"clinvar-raw-testdata_20210412.txt"
       io/reader
       line-seq
       ;; file-lazy-seq
@@ -91,37 +86,6 @@
                     #_(map #(xform-types/add-model-jsonld %)))]
     events))
 
-;; (defn test-add-model-stepwise []
-;;   (mount.core/start #'genegraph.database.instance/db)
-;;   (def event (->> (get-variant-messages)
-;;                   (filter #(= "12610" (get-in % [:content :id])))
-;;                   (take 1)
-;;                   (map eventify)
-;;                   (map genegraph.transform.clinvar.core/add-parsed-value)
-;;                   first))
-;;   (-> event
-;;       :genegraph.transform.clinvar.core/parsed-value
-;;       variation/variation-preprocess
-;;       (#(do (pprint %) %))
-;;       variation/add-variation-triples
-;;       (#(do (log/info :triples (:genegraph.transform.clinvar.variation/triples %)) %))
-;;       (#(do (pprint (:genegraph.transform.clinvar.variation/triples %)) %))
-;;       (#(do (def with-triples %) %))
-;;       ((fn [annotated-message]
-;;          (let [{triples :genegraph.transform.clinvar.variation/triples
-;;                 deferred-triples :genegraph.transform.clinvar.variation/deferred-triples}
-;;                annotated-message]
-;;            (assoc annotated-message
-;;                   :genegraph.transform.clinvar.variation/combined-triples
-;;                   (concat triples
-;;                           (for [deferred-triple deferred-triples]
-;;                             (let [{generator :generator} deferred-triple]
-;;                               (let [realized (generator)]
-;;                                 (log/info :realized realized)
-;;                                 realized))))))))
-;;       (#(do (def with-combined-triples %) %))))
-
-
 (defn test-event-process! [variant-message]
   (-> variant-message
       (eventify)
@@ -139,30 +103,19 @@
 (defn test-event-process-jsonld! [variant-message]
   (-> variant-message
       (eventify)
-      ;; (genegraph.transform.clinvar.core/add-parsed-value)
       (#(ann/add-metadata %))
       (#(xform-types/add-model %))
       (#(ann/add-iri %))
       (action/add-action)
-      ;; (#(do (log/info :event %) %))
-      (#(event/add-to-db! %))
-      #_(#((:enter ser/add-graphql-params-interceptor) %))
-      #_(#((:enter ser/add-graphql-serialization-interceptor) %))
-      #_(#((:leave event-recorder/record-event-interceptor) %))))
+      (#(event/add-to-db! %))))
 
 (defn message-proccess-no-db! [variant-message]
   (-> variant-message
       (eventify)
       (#(ann/add-metadata %))
-      (#(xform-types/add-model %))
-      #_(#(ann/add-iri %))
-      #_(action/add-action)
-      #_(#(event/add-to-db! %))
-      #_(#((:enter ser/add-graphql-params-interceptor) %))
-      #_(#((:enter ser/add-graphql-serialization-interceptor) %))
-      #_(#((:leave event-recorder/record-event-interceptor) %))))
+      (#(xform-types/add-model %))))
 
-(defn hmmm []
+(defn -write-graphql-from-db []
   (start-states!)
   (with-open [;;reader (io/reader "clinvar-raw-filtered.txt")
               ]
@@ -193,9 +146,7 @@
                           :iri (:genegraph.annotate/iri processed-event))
                (do (log/info :msg "Writing %d bytes of JSON" :count (count graphql-json))
                    (.write writer (-> processed-event get-graphql-json))
-                   (.write writer "\n")))))))))
-  ;;
-  )
+                   (.write writer "\n"))))))))))
 
 
 (defn replace-kvs
