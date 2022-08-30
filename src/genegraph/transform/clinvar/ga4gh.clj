0daa6334b290f527a7ebc536dc541502c961822b
Kyle Ferriter <kferrite@broadinstitute.org>
2022-08-30 16:13:29 -0400

    Adding repl/quick-transform module for outputting ga4gh models

---
+++ src/genegraph/transform/clinvar/ga4gh.clj
@@ -0,0 +1,46 @@
+(ns genegraph.transform.clinvar.ga4gh
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [genegraph.annotate :as ann]
+            [genegraph.transform.types :as xform-types]
+            [mount.core :as mount]))
+
+
+(defn start-states! []
+  (mount/start
+   #'genegraph.database.instance/db
+   #'genegraph.database.property-store/property-store
+   #'genegraph.transform.clinvar.cancervariants/vicc-db
+   #'genegraph.sink.event-recorder/event-database))
+
+(defn eventify [input-map]
+  ;; Mostly replicating
+  ;; (map #(stream/consumer-record-to-clj % :clinvar-raw))
+  ;; but without some fields that are not used here
+  {::ann/format :clinvar-raw
+   :genegraph.sink.event/key nil
+   :genegraph.sink.event/value (json/generate-string input-map)
+   ::topic "clinvar-raw"
+   ::partition 0})
+
+(def topic-file "clinvar-raw-local-filtered-vcepvars.txt")
+
+(defn get-message-seq []
+  (-> topic-file
+      io/reader
+      line-seq
+      (->> (map #(json/parse-string % true)))))
+
+(defn message-proccess-no-db!
+  "Takes a message value map. The :value of a KafkaRecord, parsed as json"
+  [message]
+  (-> message
+      (eventify)
+      (#(ann/add-metadata %))
+      (#(xform-types/add-data %))
+      #_(#(xform-types/add-model %))))
+
+(defn process-topic-file [input-filename]
+  (let [messages (map #(json/parse-string % true) (line-seq (io/reader input-filename)))]
+    (map message-proccess-no-db! messages)))
