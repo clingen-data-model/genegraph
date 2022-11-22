f4ff15ffd9e8878a41b8990290f7849dfc2bb5be
Kyle Ferriter <kferrite@broadinstitute.org>
2022-11-22 14:44:57 -0500

    Add main and main_repl. Change build.clj to only compile main src tree.

--- build.clj
+++ build.clj
@@ -1,13 +1,12 @@
 (ns build
   "Build this thing."
-  (:require [clojure.pprint          :refer [pprint]]
-            [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]))
 
 (def defaults
   "The defaults to configure a build."
   {:class-dir  "target/classes"
    :java-opts  ["-Dclojure.main.report=stderr"]
-   :main       'genegraph.server
+   :main       'genegraph.main
    :path       "target"
    :project    "deps.edn"
    :target-dir "target/classes"
@@ -20,5 +19,7 @@
         project                   (assoc defaults :basis basis)]
     (b/delete      project)
     (b/copy-dir    (assoc project :src-dirs paths))
-    (b/compile-clj (assoc project :src-dirs ["src"]))
+    (b/compile-clj (assoc project
+                          :src-dirs ["src"]
+                          :ns-compile ['genegraph.main]))
     (b/uber        project)))
--- src/genegraph/transform/clinvar/hgvs.clj
+++ src/genegraph/transform/clinvar/hgvs.clj
@@ -1,10 +1,6 @@
 (ns genegraph.transform.clinvar.hgvs
   (:require [clojure.pprint :refer [pprint]]))
 
-(use 'clojure.repl)
-
-
-
 (def sequence-info-re #"(.+)?:(.+)?\.(.*)")
 
 ; pattern for everything after the c./g./etc
@@ -16,22 +12,53 @@
    "NC_000017.10:g.(?_34508117)_(36248918_?)dup"
    "NC_000021.7:g.(40550036_40589822)_(46915388_46944323)del"])
 
-(pprint
- (for [input-expresion expressions]
-   (let [[matching-substring
-          accession
-          sequence-type
-          remainder]
-         (re-find (re-pattern sequence-info-re) input-expresion)]
-     (-> {:matching-substring matching-substring
-          :accession accession
-          :sequence-type sequence-type
-          :remainder remainder}
-         (#(if-let [start-range (re-find (re-pattern (str "^" coord-range-re "_" "(.+)")) (:remainder %))]
-             (assoc %
-                    :start (-> start-range rest drop-last)
-                    :remainder (last start-range))
-             (assoc % :start (second (re-find #"(\d+)_" remainder)))))
-         (#(if-let [end-range (re-find (re-pattern (str coord-range-re)) (:remainder %))]
-             (assoc % :end (rest end-range))
-             %))))))
+(def totally-invalid
+  ["asdf"])
+
+(defn hgvs-parse-sequence-and-location
+  "Attempts to parse out HGVS terms from input-expression.
+   {:accession The NC_... expression name
+    :sequence-type g, c, m, etc
+    :start Either an integer, a question mark, or a vector of two of either. 123, [123, ?], etc
+    :end The same as :start
+    :remainder Everything after the end location. A substitution, del, dup
+    :input-expression The original expression passed in}"
+  [input-expression]
+  (letfn [(add-start [{:keys [remainder] :as %}]
+            (if-let [start-range (re-find (re-pattern (str "^" coord-range-re "_" "(.+)")) remainder)]
+              (assoc %
+                     :start (-> start-range rest drop-last)
+                     :remainder (last start-range))
+              (assoc %
+                     :start (nth (re-find #"(\d+)_" remainder) 1)
+                     :remainder (nth (re-find #"(\d+)_(.*)" remainder) 2))))
+          (add-end [{:keys [remainder] :as %}]
+            (if-let [end-range (re-find (re-pattern (str coord-range-re "(.*)")) remainder)]
+              (assoc %
+                     :end (-> end-range rest drop-last)
+                     :remainder (-> end-range last))
+              (assoc %
+                     :end (nth (re-find #"(\d+)" remainder) 1)
+                     :remainder (nth (re-find #"(\d+)(.*)" remainder) 2))))]
+    (let [[matching-substring
+           accession
+           sequence-type
+           remainder]
+          (re-find (re-pattern sequence-info-re) input-expression)]
+      (some-> matching-substring
+              (#(identity {:expression input-expression
+                           :matching-substring %
+                           :accession accession
+                           :sequence-type sequence-type
+                           :remainder remainder}))
+              add-start
+              add-end))))
+
+(comment
+  (pprint
+   (for [expr expressions]
+     (hgvs-parse-sequence-and-location expr)))
+
+  (pprint
+   (for [expr totally-invalid]
+     (hgvs-parse-sequence-and-location expr))))
