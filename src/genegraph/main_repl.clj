7d183f7342029bb17a62fe3418077434b056447e
Kyle Ferriter <kferrite@broadinstitute.org>
2023-01-01 15:15:37 -0500

    Make nrepl state only start if port envvar is defined

--- src/genegraph/main_repl.clj
+++ src/genegraph/main_repl.clj
@@ -8,14 +8,16 @@
 ;; https://cljdoc.org/d/nrepl/nrepl/1.0.0/doc/usage/server#_embedding_nrepl
 
 (mount/defstate nrepl-server
-  :start (nrepl.server/start-server :port (-> "GENEGRAPH_NREPL_PORT"
-                                              System/getenv
-                                              (or "6000")
-                                              parse-long))
-  :stop (nrepl.server/stop-server nrepl-server))
+  :start (when (System/getenv "GENEGRAPH_NREPL_PORT")
+           (log/info :msg "Starting nrepl server")
+           (nrepl.server/start-server :port (-> "GENEGRAPH_NREPL_PORT"
+                                                System/getenv
+                                                parse-long)
+                                    ;; Add host 127.0.0.1
+                                      ))
+  :stop (when nrepl-server
+          (nrepl.server/stop-server nrepl-server)))
 
 (defn -main [& args]
-  (when (seq (System/getenv "GENEGRAPH_NREPL_PORT"))
-    (log/info :msg "Starting nrepl server")
-    (mount/start #'nrepl-server))
+  (mount/start #'nrepl-server)
   (apply genegraph.main/-main args))
