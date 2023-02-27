4985f7e6f8427f1ab9755c51783bace0b3907834
Kyle Ferriter <kferrite@broadinstitute.org>
2023-02-27 14:30:31 -0500

    Add a main command to just start a repl and do nothing else

--- src/genegraph/main_repl.clj
+++ src/genegraph/main_repl.clj
@@ -7,8 +7,10 @@
 
 ;; https://cljdoc.org/d/nrepl/nrepl/1.0.0/doc/usage/server#_embedding_nrepl
 
+(def nrepl-port (System/getenv "GENEGRAPH_NREPL_PORT"))
+
 (mount/defstate nrepl-server
-  :start (when (System/getenv "GENEGRAPH_NREPL_PORT")
+  :start (when nrepl-port
            (log/info :msg "Starting nrepl server")
            (nrepl.server/start-server :port (-> "GENEGRAPH_NREPL_PORT"
                                                 System/getenv
@@ -18,6 +20,18 @@
   :stop (when nrepl-server
           (nrepl.server/stop-server nrepl-server)))
 
+(comment
+  ; repl-only mode is enable just running a repl into this code or the compiled jar that does nothing
+  (clojure.string/join " "
+                       ["kubectl" "run"
+                        "--image=gcr.io/clingen-dev/genegraph:vrs-cache"
+                        "--image-pull-policy=Always"
+                        "--env=GENEGRAPH_NREPL_PORT=6000"
+                        "my-genegraph-repl"
+                        "--" "java" "-jar" "-Xmx256m" "/app/app.jar" "nrepl-only"]))
 (defn -main [& args]
+  (log/info :args args)
   (mount/start #'nrepl-server)
-  (apply genegraph.main/-main args))
+  (if (= "repl-only" (first args))
+    (while true (Thread/sleep (* 60 60 1000)))
+    (apply genegraph.main/-main args)))
