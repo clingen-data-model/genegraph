f24b3394bfbe70f331bfbf76e1299b4a0aa7919e
Kyle Ferriter <kferrite@broadinstitute.org>
2023-04-11 14:00:09 -0400

    Change loop to while/let, refactor write/upload context to indicate the directory prefix in the local data volume and bucket. Limit input size for debugging

--- src/genegraph/source/snapshot/core.clj
+++ src/genegraph/source/snapshot/core.clj
@@ -105,7 +105,6 @@
    #'genegraph.database.property-store/property-store
    #'genegraph.transform.clinvar.cancervariants/cache-db
    #'genegraph.transform.clinvar.variation/variation-data-db
-   #'genegraph.transform.clinvar.variation/variation-data-db
    #'genegraph.transform.clinvar.clinical-assertion/trait-data-db
    #'genegraph.transform.clinvar.clinical-assertion/trait-set-data-db
    #'genegraph.transform.clinvar.clinical-assertion/clinical-assertion-data-db
@@ -117,36 +116,64 @@
 
 (defn write-snapshots
   "DATASETS is a map of output filename to the rocksdb object it's content will be based on
-   {\"variation.ndjson\" myvariation-snapshot-db}"
+   {\"variation.ndjson\" myvariation-snapshot-db}
+
+   OPTIONS can be:
+   - :output-vol : the root location on the filesystem to export snapshots to.
+       For example a mountpoint, or genegraph data-vol
+       Defaults to env/data-vol.
+   - :output-prefix : within :output-vol, the directory to export *this* snapshot job to.
+       For example a directory with the current timestamp, data version, or code version.
+       Defaults to snapshots/<current-iso-timestamp>
+
+   Returns a map with :directory and :files.
+   Directory is the top level directory for the outputs of this job.
+   Files are the file paths under that directory, which may have multiple sub-directories.
+   Example:
+
+   {:output-vol \"/data/2021-11-19T2032\",
+   :output-prefix \"snapshots/2023-04-10T23:44:14.850519Z\",
+   :files
+   [\"variation-descriptors.ndjson.gz\"
+    \"statements.ndjson.gz\"]}
+  "
   ([datasets] (write-snapshots datasets {}))
-  ([datasets {:keys [gzip] :as options}]
-   (let [start (Instant/now)]
+  ([datasets {:keys [gzip output-vol output-prefix]
+              :or {gzip false
+                   output-vol env/data-vol
+                   output-prefix (str "snapshots/" (Instant/now))}
+              :as options}]
+   (let [output-full-directory (ga4gh/slashjoin output-vol output-prefix)
+         start (Instant/now)]
+     (fs/mkdirs output-full-directory)
      (loop [datasets datasets
-            outputs {}]
+            output {:output-vol output-vol
+                    :output-prefix output-prefix
+                    :files []}]
        (if (empty? datasets)
-         outputs
+         output
          (let [[filename db] (first datasets)
-               rel-dir (str "snapshots/" (str start))
-               rel-path (-> (str rel-dir "/" filename)
-                            (cond-> gzip (str ".gz")))]
-           (log/info :fn :write-snapshots :timestamp (str start) :path rel-path)
-           (fs/mkdirs rel-dir)
+               db (if (var? db) (var-get db) db)
+               filename (cond-> filename gzip (str ".gz"))
+               vol-path (str output-prefix "/" filename)
+               full-path (str output-vol "/" vol-path)]
+           (log/info :fn :write-snapshots :timestamp (str start) :path vol-path)
            (with-open [writer (if gzip
-                                (gzip-file-writer rel-path)
-                                (io/writer rel-path))]
+                                (gzip-file-writer full-path)
+                                (io/writer full-path))]
              (doseq [[i [entry-k entry-v]] (map-indexed vector (ga4gh/latest-records db))]
                (when (= 0 (rem i 1000))
                  (log/info :snapshot filename :progress i))
                (.write writer (json/generate-string entry-v))
                (.write writer "\n")))
            (recur (rest datasets)
-                  (assoc outputs filename rel-path))))))))
+                  (update output :files conj filename))))))))
 
 (def snapshot-datasets
   {"variation-descriptors.ndjson"
-   genegraph.transform.clinvar.variation/variation-data-db
+   #'genegraph.transform.clinvar.variation/variation-data-db
    "statements.ndjson"
-   genegraph.transform.clinvar.clinical-assertion/clinical-assertion-data-db})
+   #'genegraph.transform.clinvar.clinical-assertion/clinical-assertion-data-db})
 
 (defn write-snapshot-outputs-to-bucket
   "Takes a map in the form returned by write-snapshots, copies them to the configured bucket
@@ -156,13 +183,16 @@
    {basename relative-file-path}
 
    {\"variation.ndjson\" \"snapshots/2023-04-05T12:01:05.0Z/variation.ndjson\"}"
-  [snapshot-file-map]
-  (doseq [[_ output-filename] snapshot-file-map]
-    (log/info :msg "Writing file to bucket"
-              :local-file output-filename
-              :bucket-file output-filename
-              :bucket env/genegraph-bucket)
-    (gcs/put-file-in-bucket! output-filename output-filename)))
+  [{:keys [output-vol output-prefix files]
+    :as snapshot-output-map}]
+  (doseq [filename files]
+    (let [local-path (ga4gh/slashjoin output-vol output-prefix filename)
+          path-in-bucket (ga4gh/slashjoin output-prefix filename)]
+      (log/info :msg "Writing file to bucket"
+                :local-file local-path
+                :bucket-file path-in-bucket
+                :bucket env/genegraph-bucket)
+      (gcs/put-file-in-bucket! local-path path-in-bucket))))
 
 (defn process-event-catch-exceptions [event]
   (try (ev/process-event! event)
@@ -183,7 +213,8 @@
     (with-open [c (stream/consumer-for-topic topic-kw)]
       (let [tps (stream/topic-partitions c topic-kw)
             tp (first tps)
-            end (-> (.endOffsets c [tp]) first val)]
+            end (-> (.endOffsets c [tp]) first val)
+            end (min 100000 end)]
         (when (< 1 (count tps))
           (log/error :msg "Not implemented for multi-partition topics!")
           (throw (ex-info "Not implemented for multi-partition topics!" {:tps tps})))
@@ -194,43 +225,43 @@
                                (mapv #(vector [(.topic (key %)) (.partition (key %))]
                                               (val %)))
                                (into {})))
-        (loop [records (stream/poll-once c)]
-          (when @keep-running-atom
-            (let [start (Instant/now)]
-              (dorun (cp/pmap
-                      thread-pool
-                      (fn [record]
-                        (let [start (Instant/now)]
-                          (when @keep-running-atom
-                            (let [event (-> record
-                                            (stream/consumer-record-to-event topic-kw)
-                                            (assoc ::ev/interceptors interceptor-chain)
-                                            (process-event-catch-exceptions))]
-                              (let [end (Instant/now)
-                                    dur (Duration/between start end)]
-                                (when (< 0 (.compareTo dur (Duration/ofMillis 200)))
-                                  (log/warn :msg "process-event took longer than 200 milliseconds"
-                                            :duration (str dur)
-                                            :event-data (:genegraph.annotate/data event))))))))
-                      records))
-              (log/info :progress (str (.position c tp) "/" end)
-                        :batch-size (count records)
-                        :batch-duration (str (Duration/between start (Instant/now)))
-                        :average-duration (when (not= 0 (count records))
-                                            (str (.dividedBy (Duration/between start (Instant/now))
-                                                             (count records)))))
-              (when (< (.position c tp) end)
-                (recur (stream/poll-once c))))))))
+        (while (and (< (.position c tp) end)
+                    @keep-running-atom)
+          (let [records (stream/poll-once c)
+                start (Instant/now)]
+            (dorun (cp/pmap
+                    thread-pool
+                    (fn [record]
+                      (let [start (Instant/now)]
+                        (when @keep-running-atom
+                          (let [event (-> record
+                                          (stream/consumer-record-to-event topic-kw)
+                                          (assoc ::ev/interceptors interceptor-chain)
+                                          (process-event-catch-exceptions))]
+                            (let [end (Instant/now)
+                                  dur (Duration/between start end)]
+                              (when (< 0 (.compareTo dur (Duration/ofMillis 200)))
+                                (log/warn :msg "process-event took longer than 200 milliseconds"
+                                          :duration (str dur)
+                                          :event-data (:genegraph.annotate/data event))))))))
+                    records))
+            (log/info :progress (str (.position c tp) "/" end)
+                      :batch-size (count records)
+                      :batch-duration (str (Duration/between start (Instant/now)))
+                      :average-duration (when (not= 0 (count records))
+                                          (str (.dividedBy (Duration/between start (Instant/now))
+                                                           (count records)))))))))
     (if @keep-running-atom
       (do (log/info :msg "Done reading topic" :topic topic-kw)
-          (let [output-file-map (write-snapshots snapshot-datasets {:gzip true})]
-            (log/info :msg "Finished writing snapshot files" :files (mapv second output-file-map))
+          (let [{output-directory :directory
+                 output-files :files
+                 :as output-file-map} (write-snapshots snapshot-datasets {:gzip true})]
+            (log/info :msg "Finished writing snapshot files" :files output-files)
             (when env/snapshot-upload
               (log/info :msg "Uploading snapshot outputs to bucket"
                         :bucket env/genegraph-bucket
-                        :files (mapv second output-file-map))
+                        :files output-files)
               (write-snapshot-outputs-to-bucket output-file-map))))
-
       (log/warn :msg "Ended snapshot due to caller signal"))))
 
 (defonce snapshot-keep-running-atom (atom true))
